dc74ee945ad9744c35476b3d6d96734f95401086
reneeb <github@renee-baecker.de>
2014-10-29 10:56:55 +0100

    add templates for ueb_13

---
+++ ueb_13/TicketAttachments.pm
@@ -0,0 +1,222 @@
+# --
+# TicketAttachments.pm - code to excecute during package installation
+# Copyright (C) 2013 Perl-Services.de, http://perl-services.de
+# --
+# This software comes with ABSOLUTELY NO WARRANTY. For details, see
+# the enclosed file COPYING for license information (AGPL). If you
+# did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
+# --
+
+package var::packagesetup::TicketAttachments;
+
+use strict;
+use warnings;
+
+use Kernel::Config;
+use Kernel::System::SysConfig;
+
+use vars qw(@ISA $VERSION);
+$VERSION = qw($Revision: 1.12 $) [1];
+
+=head1 NAME
+
+TicketAttachments.pm - code to excecute during package installation
+
+=head1 SYNOPSIS
+
+All functions
+
+=head1 PUBLIC INTERFACE
+
+=over 4
+
+=cut
+
+=item new()
+
+create an object
+
+    use Kernel::Config;
+    use Kernel::System::Encode;
+    use Kernel::System::Log;
+    use Kernel::System::Main;
+    use Kernel::System::Time;
+    use Kernel::System::DB;
+    use Kernel::System::XML;
+    use var::packagesetup::TicketAttachments;
+
+    my $ConfigObject = Kernel::Config->new();
+    my $EncodeObject = Kernel::System::Encode->new(
+        ConfigObject => $ConfigObject,
+    );
+    my $LogObject    = Kernel::System::Log->new(
+        ConfigObject => $ConfigObject,
+        EncodeObject => $EncodeObject,
+    );
+    my $MainObject = Kernel::System::Main->new(
+        ConfigObject => $ConfigObject,
+        EncodeObject => $EncodeObject,
+        LogObject    => $LogObject,
+    );
+    my $TimeObject = Kernel::System::Time->new(
+        ConfigObject => $ConfigObject,
+        LogObject    => $LogObject,
+    );
+    my $DBObject = Kernel::System::DB->new(
+        ConfigObject => $ConfigObject,
+        EncodeObject => $EncodeObject,
+        LogObject    => $LogObject,
+        MainObject   => $MainObject,
+    );
+    my $CodeObject = var::packagesetup::TicketAttachments->new(
+        ConfigObject => $ConfigObject,
+        EncodeObject => $EncodeObject,
+        LogObject    => $LogObject,
+        MainObject   => $MainObject,
+        DBObject     => $DBObject,
+        TimeObject   => $TimeObject,
+    );
+
+=cut
+
+sub new {
+    my ( $Type, %Param ) = @_;
+
+    # allocate new hash for object
+    my $Self = {};
+    bless( $Self, $Type );
+
+    # check needed objects
+    for my $Object (
+        qw(ConfigObject EncodeObject LogObject MainObject DBObject TimeObject)
+        )
+    {
+        $Self->{$Object} = $Param{$Object} || die "Got no $Object!";
+    }
+
+    # create needed sysconfig object
+    $Self->{SysConfigObject}    = Kernel::System::SysConfig->new( %{$Self} );
+
+    # rebuild ZZZ* files
+    $Self->{SysConfigObject}->WriteDefault();
+
+    # define the ZZZ files
+    my @ZZZFiles = (
+        'ZZZAAuto.pm',
+        'ZZZAuto.pm',
+    );
+
+    # reload the ZZZ files (mod_perl workaround)
+    for my $ZZZFile (@ZZZFiles) {
+
+        PREFIX:
+        for my $Prefix (@INC) {
+            my $File = $Prefix . '/Kernel/Config/Files/' . $ZZZFile;
+            next PREFIX if !-f $File;
+            do $File;
+            last PREFIX;
+        }
+    }
+
+    # create needed objects
+    $Self->{ConfigObject} = Kernel::Config->new();
+
+    return $Self;
+}
+
+=item CodeInstall()
+
+run the code install part
+
+    my $Result = $CodeObject->CodeInstall();
+
+=cut
+
+sub CodeInstall {
+    my ( $Self, %Param ) = @_;
+
+    $Self->_LoadJS();
+
+    return 1;
+}
+
+=item CodeUpgrade()
+
+run the code reinstall part
+
+    my $Result = $CodeObject->CodeUpgrade();
+
+=cut
+
+sub CodeUpgrade {
+    my ( $Self, %Param ) = @_;
+
+    $Self->_LoadJS();
+
+    return 1;
+}
+
+=item CodeReinstall()
+
+run the code reinstall part
+
+    my $Result = $CodeObject->CodeReinstall();
+
+=cut
+
+sub CodeReinstall {
+    my ( $Self, %Param ) = @_;
+
+    return 1;
+}
+
+=item CodeUninstall()
+
+run the code uninstall part
+
+    my $Result = $CodeObject->CodeUninstall();
+
+=cut
+
+sub CodeUninstall {
+    my ( $Self, %Param ) = @_;
+
+    return 1;
+}
+
+=item _LoadJS()
+
+Add special javascript to the generally loaded javascript files
+
+=cut
+
+sub _LoadJS {
+    my ($Self, %Param) = @_;
+
+    my $CommonJS = $Self->{ConfigObject}->Get('Loader::Agent::CommonJS') || {};
+    my @JSFiles  = @{ $CommonJS->{'000-Framework'} || [] };
+
+    my $File = 'PS.TicketAttachment.Search.js';
+    push @JSFiles, $File if !grep{ $_ eq $File }@JSFiles;
+
+    # update the sysconfig
+    my $Success = $Self->{SysConfigObject}->ConfigItemUpdate(
+        Valid => 1,
+        Key   => 'Loader::Agent::CommonJS###000-Framework',
+        Value => \@JSFiles,
+    );
+}
+   
+
+1;
+
+=back
+
+=head1 TERMS AND CONDITIONS
+
+This software comes with ABSOLUTELY NO WARRANTY. For details, see
+the enclosed file COPYING for license information (GPL). If you
+did not receive this file, see L<http://www.gnu.org/licenses/gpl-2.0.txt>.
+
+=cut
+
