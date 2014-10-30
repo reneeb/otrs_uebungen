ae956ec5c85ada7030f96b1e798819f8c1612f63
reneeb <github@renee-baecker.de>
2014-10-30 15:15:14 +0100

    add stub for outputfilter

---
+++ ueb_21/OutputFilterClose.pm
@@ -0,0 +1,46 @@
+# --
+# Kernel/Output/HTML/OutputFilterClose.pm
+# Copyright (C) 2011 Perl-Services.de, http://www.perl-services.de/
+# --
+# This software comes with ABSOLUTELY NO WARRANTY. For details, see
+# the enclosed file COPYING for license information (AGPL). If you
+# did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
+# --
+
+package Kernel::Output::HTML::OutputFilterClose;
+
+use strict;
+use warnings;
+
+sub new {
+    my ( $Type, %Param ) = @_;
+
+    # allocate new hash for object
+    my $Self = {};
+    bless( $Self, $Type );
+
+    # get needed objects
+    for my $Object (
+        qw(MainObject ConfigObject LogObject LayoutObject ParamObject)
+        )
+    {
+        $Self->{$Object} = $Param{$Object} || die "Got no $Object!";
+    }
+
+    return $Self;
+}
+
+sub Run {
+    my ( $Self, %Param ) = @_;
+
+    # get template name
+    my $Templatename = $Param{TemplateFile} || '';
+    return 1 if !$Templatename;
+    return 1 if !$Param{Templates}->{$Templatename};
+
+    ${ $Param{Data} } =~ s{(<label>\$Text{"CustomerID"})}{<label>\$Text{"Customer"}}mgs;
+
+    return ${ $Param{Data} };
+}
+
+1;
