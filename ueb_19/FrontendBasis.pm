953ae03b357a6d334dc7ffb7d136bf11c28a7b99
reneeb <github@renee-baecker.de>
2014-10-30 08:52:18 +0100

    add stub for frontend module

---
+++ ueb_19/FrontendBasis.pm
@@ -0,0 +1,39 @@
+package Kernel::Modules::FrontendBasis;
+
+use strict;
+use warnings;
+
+sub new {
+    my ( $Type, %Param ) = @_;
+
+    # allocate new hash for object
+    my $Self = {%Param};
+    bless( $Self, $Type );
+
+    # check all needed objects
+    for my $Needed ( qw(ParamObject DBObject QueueObject LayoutObject ConfigObject LogObject)) {
+        if ( !$Self->{$Needed} ) {
+            $Self->{LayoutObject}->FatalError( 
+                Message => "Got no $Needed!",
+            );
+        }
+    }
+
+    # create needed objects - don't forget to "use" the module
+    # $Self->{NeededObject} = Kernel::System::Needed->new( %Param );
+
+    return $Self;
+}
+
+sub Run {
+    my ($Self, %Param) = @_;
+
+    my $Output = $Self->{LayoutObject}->Header();
+    $Output .= $Self->{LayoutObject}->NavigationBar();
+    # $Output .= $Self->{LayoutObject}->Output(
+    #     TemplateFile => 'FrontendBasis',
+    #     Data         => {
+    #     },
+    # );
+    $Output .= $Self->{LayoutObject}->Footer();
+}
