8d1e4fbf4a8109d38062489d9a2fe7dd76536281
reneeb <github@renee-baecker.de>
2014-10-31 15:18:46 +0100

    add DF.pm

---
+++ DynamicFields.pm
@@ -0,0 +1,345 @@
+# --
+# DynamicFields.pm - code to excecute during package installation
+# Copyright (C) 2014 Perl-Services.de, http://perl-services.de
+# --
+# This software comes with ABSOLUTELY NO WARRANTY. For details, see
+# the enclosed file COPYING for license information (AGPL). If you
+# did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
+# --
+
+package var::packagesetup::DynamicFields;
+
+use strict;
+use warnings;
+
+use utf8;
+
+use List::Util qw(first);
+
+use Kernel::Config;
+use Kernel::System::SysConfig;
+use Kernel::System::Valid;
+use Kernel::System::DynamicField;
+
+our $VERSION = 0.01;
+
+=head1 NAME
+
+DynamicFields.pm - code to excecute during package installation
+
+=head1 SYNOPSIS
+
+All functions
+
+=head1 PUBLIC INTERFACE
+
+=over 4
+
+=cut
+
+=item new()
+
+create an object
+
+    use Kernel::Config;
+    use Kernel::System::Encode;
+    use Kernel::System::Log;
+    use Kernel::System::Main;
+    use Kernel::System::Time;
+    use Kernel::System::DB;
+    use var::packagesetup::DynamicFields;
+
+    my $ConfigObject = Kernel::Config->new();
+    my $EncodeObject = Kernel::System::Encode->new(
+        ConfigObject => $ConfigObject,
+    );
+    my $LogObject    = Kernel::System::Log->new(
+        ConfigObject => $ConfigObject,
+        EncodeObject => $EncodeObject,
+    );
+    my $MainObject = Kernel::System::Main->new(
+        ConfigObject => $ConfigObject,
+        EncodeObject => $EncodeObject,
+        LogObject    => $LogObject,
+    );
+    my $TimeObject = Kernel::System::Time->new(
+        ConfigObject => $ConfigObject,
+        LogObject    => $LogObject,
+    );
+    my $DBObject = Kernel::System::DB->new(
+        ConfigObject => $ConfigObject,
+        EncodeObject => $EncodeObject,
+        LogObject    => $LogObject,
+        MainObject   => $MainObject,
+    );
+    my $CodeObject = var::packagesetup::DynamicFields->new(
+        ConfigObject => $ConfigObject,
+        EncodeObject => $EncodeObject,
+        LogObject    => $LogObject,
+        MainObject   => $MainObject,
+        TimeObject   => $TimeObject,
+        DBObject     => $DBObject,
+    );
+
+=cut
+
+sub new {
+    my ( $Type, %Param ) = @_;
+
+    # allocate new hash for object
+    my $Self = {};
+    bless( $Self, $Type );
+
+    # check needed objects
+    for my $Object (
+        qw(ConfigObject EncodeObject LogObject MainObject TimeObject DBObject)
+        )
+    {
+        $Self->{$Object} = $Param{$Object} || die "Got no $Object!";
+    }
+
+    # create needed sysconfig object
+    $Self->{SysConfigObject} = Kernel::System::SysConfig->new( %{$Self} );
+
+    # rebuild ZZZ* files
+    $Self->{SysConfigObject}->WriteDefault();
+
+    # define the ZZZ files
+    my @ZZZFiles = (
+        'ZZZAAuto.pm',
+        'ZZZAuto.pm',
+    );
+
+    # reload the ZZZ files (mod_perl workaround)
+    for my $ZZZFile (@ZZZFiles) {
+
+        PREFIX:
+        for my $Prefix (@INC) {
+            my $File = $Prefix . '/Kernel/Config/Files/' . $ZZZFile;
+            next PREFIX if !-f $File;
+            do $File;
+            last PREFIX;
+        }
+    }
+
+    # create needed objects
+    $Self->{ConfigObject}       = Kernel::Config->new();
+    $Self->{ValidObject}        = Kernel::System::Valid->new( %{$Self} );
+    $Self->{DynamicFieldObject} = Kernel::System::DynamicField->new( %{$Self} );
+
+    return $Self;
+}
+
+=item CodeInstall()
+
+run the code install part
+
+    my $Result = $CodeObject->CodeInstall();
+
+=cut
+
+sub CodeInstall {
+    my ( $Self, %Param ) = @_;
+
+    # create dynamic fields 
+    $Self->_CreateDynamicFields();
+    $Self->_DoSysConfigChanges();
+
+    return 1;
+}
+
+=item CodeReinstall()
+
+run the code reinstall part
+
+    my $Result = $CodeObject->CodeReinstall();
+
+=cut
+
+sub CodeReinstall {
+    my ( $Self, %Param ) = @_;
+
+    return 1;
+}
+
+=item CodeUpgrade()
+
+run the code upgrade part
+
+    my $Result = $CodeObject->CodeUpgrade();
+
+=cut
+
+sub CodeUpgrade {
+    my ( $Self, %Param ) = @_;
+
+    $Self->_CreateDynamicFields();
+    $Self->_DoSysConfigChanges();
+
+    return 1;
+}
+
+=item CodeUninstall()
+
+run the code uninstall part
+
+    my $Result = $CodeObject->CodeUninstall();
+
+=cut
+
+sub CodeUninstall {
+    my ( $Self, %Param ) = @_;
+
+    return 1;
+}
+
+=item _CreateDynamicFields()
+
+creates all dynamic fields that are necessary for reporting 
+
+    my $Result = $CodeObject->_CreateDynamicFields();
+
+=cut
+
+sub _CreateDynamicFields {
+    my ( $Self, %Param ) = @_;
+
+    my $ValidID = $Self->{ValidObject}->ValidLookup(
+        Valid => 'valid',
+    );
+
+    # get all current dynamic fields
+    my $DynamicFieldList = $Self->{DynamicFieldObject}->DynamicFieldListGet(
+        Valid => 0,
+    );
+
+    # get the list of order numbers (is already sorted).
+    my @DynamicfieldOrderList;
+    for my $Dynamicfield ( @{$DynamicFieldList} ) {
+        push @DynamicfieldOrderList, $Dynamicfield->{FieldOrder};
+    }
+
+    # get the last element from the order list and add 1
+    my $NextOrderNumber = 1;
+    if (@DynamicfieldOrderList) {
+        $NextOrderNumber = $DynamicfieldOrderList[-1] + 1;
+    }
+
+
+    # get the definition for all dynamic fields for ITSM
+    my @DynamicFields = $Self->_GetDynamicFieldsDefinition( Type => $Type );
+
+    # create a dynamic fields lookup table
+    my %DynamicFieldLookup;
+    for my $DynamicField ( @{$DynamicFieldList} ) {
+        next if ref $DynamicField ne 'HASH';
+        $DynamicFieldLookup{ $DynamicField->{Name} } = $DynamicField;
+    }
+
+    # create or update dynamic fields
+    DYNAMICFIELD:
+    for my $DynamicField (@DynamicFields) {
+
+        my $CreateDynamicField;
+
+        # check if the dynamic field already exists
+        if ( ref $DynamicFieldLookup{ $DynamicField->{Name} } ne 'HASH' ) {
+            $CreateDynamicField = 1;
+        }
+
+        # check if new field has to be created
+        if ($CreateDynamicField) {
+
+            # create a new field
+            my $FieldID = $Self->{DynamicFieldObject}->DynamicFieldAdd(
+                Name       => $DynamicField->{Name},
+                Label      => $DynamicField->{Label},
+                FieldOrder => $NextOrderNumber,
+                FieldType  => $DynamicField->{FieldType},
+                ObjectType => $DynamicField->{ObjectType},
+                Config     => $DynamicField->{Config},
+                ValidID    => $ValidID,
+                UserID     => 1,
+            );
+            next DYNAMICFIELD if !$FieldID;
+
+            # increase the order number
+            $NextOrderNumber++;
+        }
+    }
+
+    return 1;
+}
+
+=item _GetDynamicFieldsDefinition()
+
+returns the definition for reporting related dynamic fields
+
+    my $Result = $CodeObject->_GetDynamicFieldsDefinition();
+
+=cut
+
+sub _GetDynamicFieldsDefinition {
+    my ( $Self, %Param ) = @_;
+
+    # define all dynamic fields for reporting
+    my @DynamicFields = (
+        {
+          'FieldType' => 'TextArea',
+          'Name' => 'Testfeld',
+          'Label' => 'Das ist ein Testfeld',
+          'Config'    => {
+              Cols         => 40,
+              Rows         => 3,
+              DefaultValue => 'Ein Testinhalt.',
+          },
+          'ObjectType' => 'Ticket'
+        },
+    );
+
+    return @DynamicFields;
+}
+
+=item _DoSysConfigChanges()
+
+=cut
+
+sub _DoSysConfigChanges {
+    my ($Self, %Param) = @_;
+
+    my @DynamicFields = $Self->_GetDynamicFieldsDefinition( Type => $Type );
+    my %Map = map{ $_->{Name} => 1 }@DynamicFields;
+
+    my $View = $Type || 'Phone';
+    
+    for my $Option ( $View, 'Zoom' ) {
+        my $Fullname      = 'Ticket::Frontend::AgentTicket' . $Option;
+        my $Options       = $Self->{ConfigObject}->Get( $Fullname );
+        my $Mapping       = $Options->{DynamicField} || {};
+    
+        my %NewMapping = (
+            %{ $Mapping },
+            %Map,
+        );
+    
+        my $Success = $Self->{SysConfigObject}->ConfigItemUpdate(
+            Valid => 1,
+            Key   => "$Fullname###DynamicField",
+            Value => \%NewMapping,
+        );
+    }
+}
+
+
+1;
+
+=back
+
+=head1 TERMS AND CONDITIONS
+
+This software comes with ABSOLUTELY NO WARRANTY. For details, see
+the enclosed file COPYING for license information (GPL). If you
+did not receive this file, see L<http://www.gnu.org/licenses/gpl-2.0.txt>.
+
+=cut
+
